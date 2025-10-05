eda709152dd64d85cb66a8ed7ac20bbf97b51d6b
YounsooLim <tomandlim@gmail.com>
2025-10-05 15:42:08 +0900

    refactor: refactor nlp structure

---
+++ Python/tools/ai/prompts/__init__.py
@@ -0,0 +1,7 @@
+"""
+Prompt templates for AI natural language processing.
+"""
+
+from .system_prompt import get_minimal_prompt, get_full_prompt
+
+__all__ = ['get_minimal_prompt', 'get_full_prompt']
